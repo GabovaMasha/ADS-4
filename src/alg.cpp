d985b0053c72481f1383b4275a72ad8ff7cee3f3
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-03-27 00:33:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,35 +1,35 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
 int count = 0;
-for (int i = 0; i < len - 1; i++ ) {
-for (int j = i +1; j < len; j++ ) {
+for (int i = 0; i < len - 1; i++) {
+for (int j = i +1; j < len; j++) {
 if (arr[i] + arr[j] == value) {
   count++;
-  }
- }
+}
+}
 }
 return count;
 }
 int countPairs2(int *arr, int len, int value) {
 int count = 0;
-for (int i = 0; i < len - 1; i++ ) {
-for (int j = len - 1; j > i; j-- ) {
+for (int i = 0; i < len - 1; i++) {
+for (int j = len - 1; j > i; j--) {
 if (arr[i] + arr[j] == value) {
   count++;
-   }
-  }
+}
+}
 }
 return count;
 }
 int countPairs3(int *arr, int len, int value) {
 int count = 0;
-for (int a = 0; a < len - 1; a++ ) {
+for (int a = 0; a < len - 1; a++) {
 int x = a; int y = len;
 while (x < y - 1) {
 int z = x + (y - x) / 2;
 if (arr[a] + arr[z] == value) {
-   count++;
-   int b = z + 1;
+count++;
+int b = z + 1;
 while ((arr[a] + arr[b] == value) && (b < y)) {
 count++;
 b++;
